083e3eb16ece98eefa17b066c173d6b3949413cc
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-10-07 14:29:41 +0300

    done lab

--- server/app.py
+++ server/app.py
@@ -17,11 +17,29 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
-
-class PlantByID(Resource):
-    pass
+    def get(self):
+        return make_response([n.to_dict() for n in Plant.query.all()], 200)
+    
+    def post(self):
+        new_plant=Plant(
+            name = request.json['name'],
+            image = request.json['image'],
+            price = request.json['price']
+        )
+        db.session.add(new_plant)
+        db.session.commit()
         
+        return make_response(new_plant.to_dict(), 201)
+    pass
 
+api.add_resource(Plants, '/plants')
+class PlantByID(Resource):
+    def get(self, id):
+        return make_response(Plant.query.filter_by(id=id).first().to_dict(), 200)
+    pass
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+
+        
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
